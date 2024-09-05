2c081945ef6aba18146b62daed42426939dc846b
GerarC <gcastillo.m14@gmail.com>
2024-09-04 20:08:14 -0500

    initial commit
    base project made with spring initialzr

---
+++ src/main/java/co/edu/udea/securecheck/SecureCheckApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.securecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureCheckApplication.class, args);
+	}
+
+}
